a90a926478dfd36bb5f5ca39627676d4698e76c8
Ted Shu <ted.shu@ileopard.com>
2019-12-17 17:42:00 +0800

    rebuilding blog 2019年12月17日 週二 17時42分00秒 CST

--- js/custom.js
+++ js/custom.js
@@ -4,20 +4,13 @@
     social[i].querySelector('a').addEventListener('click', function (e) {
       var name = this.querySelector('span').innerHTML;
       ga('send', 'event', 'social', 'click', name);
-      gtag('event', 'click', {
-        'event_category': 'social',
-        'event_label': name
-      });
     })
   }
   var menu = document.querySelectorAll('.menu__social a');
   for (var i = 0; i < menu.length; i++) {
     menu[i].addEventListener('click', function (e) {
       var name = this.querySelector('span').innerHTML;
-      gtag('event', 'click', {
-        'event_category': 'menu',
-        'event_label': name
-      });
+      ga('send', 'event', 'menu', 'click', name);
     });
   };
 })()
