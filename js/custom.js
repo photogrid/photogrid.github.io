69b25ddb69ff17266d35068e80020a78f97e0398
Ted Shu <ted.shu@ileopard.com>
2020-03-19 15:02:58 +0800

    rebuilding blog 2020年 3月19日 週四 15時02分58秒 CST

--- js/custom.js
+++ js/custom.js
@@ -4,7 +4,7 @@
     social[i].querySelector('a').addEventListener('click', function (e) {
       var name = this.querySelector('span').innerHTML;
       ga('send', 'event', 'social', 'click', name);
-    })
+    });
   }
   var menu = document.querySelectorAll('.menu__social a');
   for (var i = 0; i < menu.length; i++) {
@@ -12,5 +12,25 @@
       var name = this.querySelector('span').innerHTML;
       ga('send', 'event', 'menu', 'click', name);
     });
-  };
+  }
+  var appbtnMenu = document.querySelectorAll('.widget-appbtn__menu a');
+  for (var i = 0; i < appbtnMenu.length; i++) {
+    appbtnMenu[i].addEventListener('click', function (e) {
+      if (this.getAttribute('data-store') === 'google_play') {
+        ga('send', 'event', 'google play', 'click', this.getAttribute('href'));
+      } else {
+        ga('send', 'event', 'apple store', 'click', this.getAttribute('href'));
+      }
+    });
+  }
+  var appbtnSidebar = document.querySelectorAll('.widget-appbtn__sidebar a');
+  for (var i = 0; i < appbtnSidebar.length; i++) {
+    appbtnSidebar[i].addEventListener('click', function (e) {
+      if (this.getAttribute('data-store') === 'google_play') {
+        ga('send', 'event', 'google play', 'click', this.getAttribute('href'));
+      } else {
+        ga('send', 'event', 'apple store', 'click', this.getAttribute('href'));
+      }
+    });
+  }
 })()
